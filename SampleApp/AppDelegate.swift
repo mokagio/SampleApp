43f4cd6c526639d62a078ac4881c7c537c9f65ec
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-03-08 10:11:30 +1100

    Sample App Initial Commit

---
+++ SampleApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SampleApp
+//
+//  Created by Giovanni Lodi on 8/03/2016.
+//  Copyright © 2016 Giovanni Lodi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Giovanni Lodi on 8/03/2016.
+//  Copyright © 2016 Giovanni Lodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
